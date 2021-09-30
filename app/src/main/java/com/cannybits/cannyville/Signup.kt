7fbcada31400118a1a5219e30f4c59d8654cd5be
nyandago <paulmutator@gmail.com>
2021-09-30 10:38:38 +0300

    Sign Up Activity

--- app/src/main/java/com/cannybits/cannyville/Signup.kt
+++ app/src/main/java/com/cannybits/cannyville/Signup.kt
@@ -2,10 +2,33 @@ package com.cannybits.cannyville
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class Signup : AppCompatActivity() {
+
+    private lateinit var uploadPhoto: Button
+    private lateinit var signUpEmail: EditText
+    private lateinit var signUpPassword: EditText
+    private lateinit var signUp : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        initView()
+
+        uploadPhoto.setOnClickListener {
+
+        }
+    }
+
+    private fun initView(){
+        uploadPhoto = btnUserImage
+        signUp = btnSignUp
+        signUpEmail = etEmailSignup
+        signUpPassword = etPasswordSignup
     }
 }
