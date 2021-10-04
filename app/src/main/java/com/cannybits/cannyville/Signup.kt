4656d2b48317cc1e61a025e5071413447c8dd250
nyandago <paulmutator@gmail.com>
2021-10-04 19:10:38 +0300

    Signup Activity

--- app/src/main/java/com/cannybits/cannyville/Signup.kt
+++ app/src/main/java/com/cannybits/cannyville/Signup.kt
@@ -37,44 +37,13 @@ class Signup : AppCompatActivity() {
         initView()
 
         uploadPhoto.setOnClickListener {
-            //checkPermission()
+
             loadImage()
 
         }
     }
 
-   /* private fun checkPermission(){
 
-        if (ActivityCompat.checkSelfPermission(this,
-            android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),UPLOADIMAGE)
-            return
-        }
-
-        loadImage()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-
-        when(requestCode){
-            UPLOADIMAGE -> {
-                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    loadImage()
-                } else{
-                    Toast.makeText(this,"Storage Permissions Denied",Toast.LENGTH_LONG).show()
-                }
-            }
-
-            else ->  super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-
-
-    }  */
 
     private fun loadImage(){
        val gallery  = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
