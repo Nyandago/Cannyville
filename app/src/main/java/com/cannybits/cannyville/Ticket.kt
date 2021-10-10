27a87ab2792bf0ad6137af958b34b20cf6f34c95
nyandago <paulmutator@gmail.com>
2021-10-10 15:16:12 +0300

    Ticket class

---
+++ app/src/main/java/com/cannybits/cannyville/Ticket.kt
@@ -0,0 +1,8 @@
+package com.cannybits.cannyville
+
+class Ticket(tweetId: String, tweetText: String, tweetImageUrl: String, tweetPersonUID: String) {
+    var tweetId:String? = tweetId
+    var tweetText: String? = tweetText
+    var tweetImageUrl: String? = tweetImageUrl
+    var tweetPersonUID: String? = tweetPersonUID
+}
