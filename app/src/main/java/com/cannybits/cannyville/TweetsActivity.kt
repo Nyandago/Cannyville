943f64a35667ee1a39b1ac902c636276efd87a9d
nyandago <paulmutator@gmail.com>
2021-10-17 20:05:20 +0300

    Tweets Activity

--- app/src/main/java/com/cannybits/cannyville/TweetsActivity.kt
+++ app/src/main/java/com/cannybits/cannyville/TweetsActivity.kt
@@ -1,17 +1,23 @@
 package com.cannybits.cannyville
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.BaseAdapter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_tweets.*
+import kotlinx.android.synthetic.main.add_tweet.view.*
 import kotlinx.android.synthetic.main.tweets_view.view.*
 import java.util.ArrayList
 
 class TweetsActivity : AppCompatActivity() {
 
     private var tweetList: ArrayList<TweetTicket> = ArrayList()
+    private var adapter:MyTweetAdapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,57 +27,49 @@ class TweetsActivity : AppCompatActivity() {
         //Dummy Data
         tweetList.add(TweetTicket("tweetId", "tweetText", "tweetImageUrl", "personUID"))
         tweetList.add(TweetTicket("000", "hello there!", "tweetImageUrl", "canny"))
+
+        adapter= MyTweetAdapter(this,tweetList)
+        lvTweets.adapter=adapter
     }
-
-    inner class StudentAdapter : RecyclerView.Adapter<StudentAdapter.TweetViewHolder>() {
-
-        private var twtList: ArrayList<TweetTicket> = ArrayList()
-        private var onClickItem: ((TweetTicket) -> Unit)? = null
-        private var onClickDeleteItem: ((TweetTicket) -> Unit)? = null
-
-        fun addItems(items: ArrayList<TweetTicket>) {
-            this.twtList = items
-            notifyDataSetChanged()
+    inner class  MyTweetAdapter: BaseAdapter {
+        var tweetsAdapter=ArrayList<TweetTicket>()
+        var context: Context?=null
+        constructor(context:Context, tweetsAdapter:ArrayList<TweetTicket>):super(){
+            this.tweetsAdapter=tweetsAdapter
+            this.context=context
+        }
+        override fun getItem(p0: Int): Any {
+            return tweetsAdapter[p0]
         }
 
-        fun setOnClickItem(callback: (TweetTicket) -> Unit) {
-            this.onClickItem = callback
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
         }
 
-        fun setOnClickDeleteItem(callback: (TweetTicket) -> Unit) {
-            this.onClickDeleteItem = callback
+        override fun getCount(): Int {
+
+            return tweetsAdapter.size
+
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = TweetViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.tweets_view, parent, false)
-        )
-
-        override fun onBindViewHolder(holder: TweetViewHolder, position: Int) {
-            val tweet = twtList[position]
-            holder.bindView(tweet)
-            holder.itemView.setOnClickListener { onClickItem?.invoke(tweet) }
-          //  holder.btnDelete.setOnClickListener { onClickDeleteItem?.invoke(tweet) }
-        }
-
-        override fun getItemCount(): Int {
-            return twtList.size
-        }
-
-        inner class TweetViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
-            private var userName = view.txtUserName
-            private var picturePath = view.iv_picture_path
-            private var tweetDate = view.txt_tweet_date
-            private var tweet = view.txt_tweet
-            //val btnDelete
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
 
 
-            fun bindView(tweet: TweetTicket) {
-              //  userName.text = tweet.id.toString()
-            //    picturePath.imag = tweet.firstName
-             //   tweetDate.text = tweet.lastName
-              //  this.tweet.text = tweet.tweetText
+            var mytweet=tweetsAdapter[p0]
+
+
+                var myView = layoutInflater.inflate(R.layout.add_tweet, null)
+
+                myView.iv_attach.setOnClickListener(View.OnClickListener {
+                  //  loadImage()
+
+                })
+                return myView
+                        }
             }
-        }
-    }
 
-}
+        }
+
+
+
+
