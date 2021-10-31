657f630894d92811b96f408a47f575a827bac5d6
nyandago <paulmutator@gmail.com>
2021-10-31 18:40:36 +0300

    Login Activity

--- app/src/main/java/com/cannybits/cannyville/Login.kt
+++ app/src/main/java/com/cannybits/cannyville/Login.kt
@@ -2,10 +2,15 @@ package com.cannybits.cannyville
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginFunction(view: View){
+
     }
 }
