a0af24de5ebe0cf86091c2b1d3682692cf40366d
nyandago <paulmutator@gmail.com>
2021-10-31 19:46:31 +0300

    Login Activity

--- app/src/main/java/com/cannybits/cannyville/Login.kt
+++ app/src/main/java/com/cannybits/cannyville/Login.kt
@@ -1,6 +1,8 @@
 package com.cannybits.cannyville
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,9 +11,14 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_login.*
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Login : AppCompatActivity() {
 
@@ -44,9 +51,6 @@ class Login : AppCompatActivity() {
         }
 
 
-
-
-
         userPhoto.setOnClickListener(
          View.OnClickListener {
              loadImage()
@@ -59,6 +63,48 @@ class Login : AppCompatActivity() {
         )
     }
 
+    private fun signUpUserToFirebase(email: String, password: String){
+        mAuth!!.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener() {
+                    task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "User Signed Up Successfully", Toast.LENGTH_LONG).show()
+                    saveImageToFirebase()
+                } else {
+                    Toast.makeText(this,"User SignUp failed", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun saveImageToFirebase(){
+        val currentUser = mAuth!!.currentUser
+        val email: String = currentUser!!.email.toString()
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.getReferenceFromUrl("gs://canny-social.appspot.com")
+        val dateFmt = SimpleDateFormat("ddMMyyHHmmss")
+        val dateObj = Date()
+        val imagePath = splitString(email) +"."+ dateFmt.format(dateObj) + ".jpg"
+        val imageRef = storageRef.child("images/"+imagePath)
+        userPhoto.isDrawingCacheEnabled = true
+        userPhoto.buildDrawingCache()
+
+        val drawable = userPhoto.drawable as BitmapDrawable
+        val bitmap = drawable.bitmap
+        val byteOs = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, byteOs)
+        val data = byteOs.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
+
+        uploadTask.addOnFailureListener{
+            Toast.makeText(this,"Failed to upload the image",Toast.LENGTH_LONG).show()
+        }
+            .addOnSuccessListener { taskSnapshot ->
+                val downloadUrl= taskSnapshot.storage.downloadUrl.toString()!!
+                mRef.child("Users").child(currentUser.uid).child("email").setValue(currentUser.email)
+                mRef.child("Users").child(currentUser.uid).child("ProfileImage").setValue(downloadUrl)
+
+            }
+    }
 
     private fun loadImage(){
         val gallery  = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
@@ -75,8 +121,10 @@ class Login : AppCompatActivity() {
         }
     }
 
-    fun loginFunction(view: View){
 
+    private fun splitString(email: String): String{
+        val split = email.split("@")
+        return split[0]
     }
 
     private fun initView(){
