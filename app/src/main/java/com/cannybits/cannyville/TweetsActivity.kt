bfe277817ccee362606237c5867728aa7d7fae72
nyandago <paulmutator@gmail.com>
2021-11-01 20:01:12 +0300

    Tweets Activity

--- app/src/main/java/com/cannybits/cannyville/TweetsActivity.kt
+++ app/src/main/java/com/cannybits/cannyville/TweetsActivity.kt
@@ -1,11 +1,15 @@
 package com.cannybits.cannyville
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.*
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -16,12 +20,23 @@ import java.util.ArrayList
 
 class TweetsActivity : AppCompatActivity() {
 
+    private lateinit var logOut : Button
+
     private var tweetList: ArrayList<TweetTicket> = ArrayList()
     private var adapter:MyTweetAdapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweets)
+
+        logOut = btnLogOut
+
+        logOut.setOnClickListener {
+            getInstance().signOut()
+
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
 
 
         //Dummy Data
