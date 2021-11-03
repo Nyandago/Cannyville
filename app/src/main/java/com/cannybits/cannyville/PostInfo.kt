673e8fec8b1622582df2fa57cbd52be32bf00278
nyandago <paulmutator@gmail.com>
2021-11-03 15:26:59 +0300

    Post Info Class

---
+++ app/src/main/java/com/cannybits/cannyville/PostInfo.kt
@@ -0,0 +1,7 @@
+package com.cannybits.cannyville
+
+class PostInfo(tweetText: String, userUid: String, imageUrl: String) {
+    var tweetText: String? = tweetText
+    var userUid: String? = userUid
+    var imageUrl: String? = imageUrl
+}
